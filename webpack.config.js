1a3a2d436e67ee34e3b0675b2f01094afe515fae
Unknown <paoolkim@gmail.com>
2018-10-24 22:02:24 -0700

    update babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,34 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const htmlWebpackPlugin = new HtmlWebpackPlugin({
-    template: path.join(__dirname, 'examples/src/index.html'),
-    filename: './index.html'
-});
+
 module.exports = {
-    entry: path.join(__dirname, 'examples/src/index.js'),
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                use: 'babel-loader',
-                exclude: /node_modules/
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            }
-        ]
-    },
-    plugins: [htmlWebpackPlugin],
-    resolve: {
-        extensions: ['.js', '.jsx']
-    },
-    devServer: {
-        port: 3001
-    }
+  entry: path.join(__dirname, 'examples/src/index.js'),
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+
+            plugins: [require('@babel/plugin-proposal-class-properties')]
+          }
+        },
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'examples/src/index.html'),
+      filename: './index.html'
+    })
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  devServer: {
+    port: 3001
+  }
 };
